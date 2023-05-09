3764957e100b3f92e2d87083c11ecdbfe311a079
Russell Sim <russell.h.sim@gmail.com>
2023-05-09 10:17:58 -0400

    Create game function, whole program works

--- javascript.js
+++ javascript.js
@@ -16,26 +16,58 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
     if (playerSelection == "rock" && computerSelection == "paper") {
-        console.log("You chose rock and computer chose paper! Computer wins!");
+        roundResult = "You chose rock and computer chose paper! You lose!";
     } else if (playerSelection == "rock" && computerSelection == "scissors") {
-        console.log("You chose rock and computer chose scissors! You win!");
+        roundResult = "You chose rock and computer chose scissors! You win!";
     } else if (playerSelection == "rock" && computerSelection == "rock") {
-        console.log("You and computer both chose rock! Draw!");
+        roundResult = ("You and computer both chose rock! Draw!");
     } else if (playerSelection == "paper" && computerSelection == "rock") {
-        console.log("You chose paper and computer chose rock! You win!");
+        roundResult = ("You chose paper and computer chose rock! You win!");
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        console.log("You chose paper and computer chose scissors! Computer wins!");
+        roundResult = "You chose paper and computer chose scissors! You lose!";
     } else if (playerSelection == "paper" && computerSelection == "paper") {
-        console.log("You and computer both chose paper! Draw!");
+        roundResult = "You and computer both chose paper! Draw!";
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
-        console.log("You chose scissors and computer chose rock! You lose!");
+        roundResult = "You chose scissors and computer chose rock! You lose!";
     } else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        console.log("You and computer both chose scissors! Draw!");
+        roundResult = "You and computer both chose scissors! Draw!";
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
-        console.log("You chose scissors and computer chose paper! You win!");
+        roundResult = "You chose scissors and computer chose paper! You win!";
+    }
+    return roundResult;
+}
+
+function game() {
+    let roundNumber = 0;
+    let playerScore = 0;
+    let computerScore = 0;
+
+    while (roundNumber < 5) {
+        let playerSelection = prompt("Make your choice!").toLowerCase();
+        let computerSelection = getComputerChoice();
+        let theResult = playRound(playerSelection, computerSelection);
+        if (theResult.includes("You win!") == true) {
+            playerScore++;
+        } else if (theResult.includes("You lose!") == true) {
+            computerScore++;
+        }
+        console.log(theResult);
+        console.log("Player: " + playerScore + " Computer: " + computerScore);
+        roundNumber++;
+    }
+
+    if  (playerScore > computerScore) {
+        console.log("You win the game!");
+    } else if (playerScore == computerScore) {
+        console.log("The game is a draw!");
+    } else {
+        console.log("Computer wins the game!")
     }
 }
 
-const playerSelection = "scissors";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+game();
+
+//For testing
+//const playerSelection = "scissors";
+//const computerSelection = getComputerChoice();
+//console.log(playRound(playerSelection, computerSelection));
